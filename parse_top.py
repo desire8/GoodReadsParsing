3031ce53cfe0f7467d2ac32725f8447cc6257afa
desire8 <sjyotisingh220@gmail.com>
2017-02-09 13:33:31 +0530

    give a list of books with rating more than 4.5

--- parse_top.py
+++ parse_top.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.goodreads.com/list'
+url = 'http://www.goodreads.com/list/show/24716.Highest_Rated_Books_on_Goodreads_with_at_least_100_ratings_'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'lxml')
 topBookList = []
 
-for div in soup.find_all("div", class_="listImgs"):
-    val = div.find('img').get('title')
+for div in soup.find_all("div", class_="js-tooltipTrigger tooltipTrigger"):
+    val = div.find('a').get('title')
     topBookList.append(val)
 print(topBookList)
